ed42d8e91c91c6ef24882de189032e5129976a3b
Junmaril Robles <junmarilr@gmail.com>
2023-01-30 09:24:40 +0800

    update

--- src/App.js
+++ src/App.js
@@ -3,24 +3,20 @@ import "./App.css";
 import { Login } from "./components/Login/Login";
 import { Dashboard } from "./components/Dashboard/Dashboard";
 import React from "react";
-import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import { Artist } from "./components/Dashboard/Artist";
 import { Songs } from "./components/Dashboard/Songs";
 import { Playlist } from "./components/Dashboard/Playlist";
+import { Route } from "react-router-dom";
 
 function App() {
   return (
-    <>
-      <Router>
-        <div>
-          <Route exact path="/login" component={Login} />
-          <Route exact path="/dashboard" component={Dashboard} />
-          <Route exact path="/artist" component={Artist} />
-          <Route exact path="/songs" component={Songs} />
-          <Route exact path="/playlist" component={Playlist} />
-        </div>
-      </Router>
-    </>
+    <div>
+      <Route exact path="/login" component={Login} />
+      <Route exact path="/dashboard" component={Dashboard} />
+      <Route exact path="/artist" component={Artist} />
+      <Route exact path="/songs" component={Songs} />
+      <Route exact path="/playlist" component={Playlist} />
+    </div>
   );
 }
 
